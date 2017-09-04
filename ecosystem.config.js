ca6607c3abb0a11c9731be989cbd3c27b12af43f
xiacan <xiac@jieshuibao.com>
2017-09-04 15:49:38 +0800

    update ecosystem deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:XcXerxes/xerxes-server-blog.git',
       path : '/home/xcxerxes/www/production',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
       user : 'xcxerxes',
