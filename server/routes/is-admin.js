ba7e5022941f7fab226d04f0c8baf22af0baf13b
XcXerxes <xcxerxes@aliyun.com>
2018-02-01 20:40:00 +0800

    update app

--- server/routes/is-admin.js
+++ server/routes/is-admin.js
@@ -2,6 +2,7 @@ const secret = require('../config/secret.json')['secret_token']
 const checkToken = require('./token')
 module.exports = (req, res, next) => {
     const token = req.body.xc_token || req.param['xc_token'] || req.headers['xc_token']
+    console.log('token', token)
     checkToken(token, {
         secret
     }, req, res, next)
